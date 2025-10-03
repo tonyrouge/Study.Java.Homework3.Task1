c925dbe95a9a33ad3dd55c8ead31a502329a1649
tonyrouge <ontone.shadrin@gmail.com>
2025-10-03 22:32:04 +0700

    Create task1.java

---
+++ task1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner( System.in );
+        System.out.printf( "Укажите ваше имя: " );
+        String name = scanner.nextLine();
+        System.out.printf("Привет, %s!\n" , name );
+    }
+}
